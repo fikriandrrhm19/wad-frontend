4ba52dc32aecde778789bb3053e93c1f444cc295
Fikri Andra Irham <fikri.andrhm@gmail.com>
2026-06-23 20:01:25 +0700

    feat: configure vite development proxy for /api and /auth endpoints

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // Semua request ke /api/... diteruskan ke backend Express (Port 3000)
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+      // Semua request ke /auth/... diteruskan ke backend Express (Port 3000)
+      "/auth": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
+  },
+});
